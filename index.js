50d7781369dde3ba4eabbf460f4f80b9005f1f83
Hamza Yehia <hamzayehiamakhlouf@gmail.com>
2022-09-29 22:03:07 +0200

    added one-letter input handling

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function reverse(form) {
+  if (form.rev.value.length == 1) {
+    console.log("Can NOT reverse one letter.");
+  }
+
   var text = "";
   var str = form.rev.value;
-  for (i = 0; i <= str.length; i++) text = str.substring(i, i + 1) + text;
+
+  for (i = 0; i <= str.length; i++) {
+    text = str.substring(i, i + 1) + text;
+  }
+
   form.rev.value = text;
 }
