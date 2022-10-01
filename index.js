abd1da4f161dff4412ff7a5975c2e71047dd9cef
Vitika Soni <vitika.program@gmail.com>
2022-10-01 15:03:02 +0530

    Console log for palindrome (#6)

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function reverse(form) {
-  if (form.rev.value.length == 1) {
-    console.log("Can NOT reverse one letter.");
+  var str = form.rev.value;
+
+  if (isPalindrome(str)) {
+    console.log("Palindrome reads the same backwards as forwards");
+    return;
   }
 
   var text = "";
-  var str = form.rev.value;
 
   for (i = 0; i <= str.length; i++) {
     text = str.substring(i, i + 1) + text;
   }
 
   form.rev.value = text;
+}
+
+function isPalindrome(str) {
+  for (i = 0; i < str.length / 2; i++) {
+    if (str.charAt(i) != str.charAt(str.length - 1 - i)) {
+      return false;
+    }
+  }
+  return true;
 }
